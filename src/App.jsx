09e8eb83240d36d5bf990d205cb63a2e82a0e368
Immanuel W <l0calh34rtz@gmail.com>
2024-12-03 16:24:41 +0700

    feat: add public routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Landing from "./components/Landing";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      {/* path = "/" -> root URL */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Landing />} />
+      </Route>
+    </Routes>
+  );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,12 @@
+const Landing = () => {
+  return (
+    <div>
+      <div>
+        <h1>ini adalah landing page</h1>
+        <p>informasi kontak si pemilik toko reparasi</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
