cb7e2f8103f78496eb372e3344d0a92fc7cc5d38
Immanuel W <l0calh34rtz@gmail.com>
2024-12-04 14:49:57 +0700

    feat: add usersList & ticketsList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Landing from "./components/Landing";
+import { UsersList } from "./pages/Users/UsersList";
+import { TicketsList } from "./pages/Tickets/TicketsList";
 
 export default function App() {
   return (
@@ -9,6 +11,15 @@ export default function App() {
       {/* path = "/" -> root URL */}
       <Route path="/" element={<Layout />}>
         <Route index element={<Landing />} />
+        <Route path="/app" element={<Layout />}>
+          <Route index element={<Navigate to="/app/users" replace />} />
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+          <Route path="tickets">
+            <Route index element={<TicketsList />} />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
